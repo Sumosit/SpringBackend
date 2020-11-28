523040ab9ac0d2b6aec18ce7fe16d829148f71f6
Sumosit <erlan99isaev@gmail.com>
2020-11-28 21:22:50 +0600

    commit

--- src/main/java/PersonalArea/backend/controller/ByPassController.java
+++ src/main/java/PersonalArea/backend/controller/ByPassController.java
@@ -21,12 +21,12 @@ public class ByPassController {
   UserRepository userRepository;
 
   @GetMapping("add/admin")
-  public String addRoleToUser(@RequestParam Long id) {
+  public User addRoleToUser(@RequestParam Long id) {
     User user = userRepository.getOne(id);
     Set<Role> roles = user.getRoles();
     roles.add(new Role(3, ERole.ROLE_ADMIN));
     user.setRoles(roles);
 //    userRepository.save(user);
-    return "user";
+    return user;
   }
 }
